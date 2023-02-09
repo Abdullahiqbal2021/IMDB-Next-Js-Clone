8e632f159e45fae87a511c1735c10511eab3711a
Abdullah Iqbal <muhammadabdullahiqbal2021@gmail.com>
2023-02-09 14:58:38 +0500

    Navbar added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <section className="max-w-6xl mx-auto">
+    <section className="max-w-6xl mx-auto p-4">
         <h1 className="text-amber-600 text-2xl font-bold my-3">About</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique inventore et dolorem quidem odio veniam suscipit corporis sit aliquid, tempora, accusantium tenetur quam cupiditate voluptatibus nostrum harum autem! Vel, aspernatur.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique inventore et dolorem quidem odio veniam suscipit corporis sit aliquid, tempora, accusantium tenetur quam cupiditate voluptatibus nostrum harum autem! Vel, aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea laudantium molestias tempore? Ullam adipisci, magnam mollitia nostrum unde aperiam iure sed repellendus saepe, molestiae porro praesentium, aut nisi? Doloremque, quasi.</p>
