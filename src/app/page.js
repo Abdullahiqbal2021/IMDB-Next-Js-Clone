79f63fd87cc4d473d9defe69d07c2eb6ce2e42dc
Abdullah Iqbal <muhammadabdullahiqbal2021@gmail.com>
2023-02-16 10:46:37 +0500

    more pages added

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,29 @@
+import Pagination from "@/components/Pagination";
 import Result from "@/components/Result";
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "top_rated";
+  const page = searchParams.page || 1;
 
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=1`,
+    `https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=${page}`,
     { next: { revalidate: 0 } }
   );
   const data = await res.json();
+  if (data.errors) {
+    return <h1 className="text-2xl text-center text-amber-500 mt-20">Page Not Found</h1>
+  }
   if (!res.ok) {
+    console.log("res is", data);
     throw new Error("Something went wrong while fetching");
   }
   // console.log(data.results);
   return (
-    <main>
+    <main className="pb-7">
       <Result results={data.results} />
+      <Pagination total_pages={data.total_pages} genre={genre} />
     </main>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,48 @@
+"use client"
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation';
+import React from 'react'
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
+
+function pageCount(page) {
+    let i = 0;
+    const Array = [];
+    while (i <= page) {
+        i++;
+        Array.push(i);
+    }
+    return Array
+}
+function next(page) {
+    return ++page
+}
+export default function Pagination({ total_pages, genre }) {
+    const searchParams = useSearchParams();
+    const page = searchParams.get("page");
+    const pages = pageCount(total_pages);
+    return (
+
+        <nav aria-label="Page navigation example" >
+            <div className="flex items-center justify-center">
+                {page >= 1 &&
+                    <Link href={`/?genre=${genre}&page=${page - 1}`} className="px-3 py-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <BsFillArrowLeftCircleFill />
+                    </Link>
+                }
+                <div className="flex max-w-sm  overflow-hidden  pag">
+                    {pages.map((item) => (
+                        <Link key={item} href={`/?genre=${genre}&page=${item}`} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            {item}
+                        </Link>
+                    ))}
+                </div>
+                {page <= pages.length &&
+                    <Link href={`/?genre=${genre}&page=${next(page)}`} className="px-3 py-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <BsFillArrowRightCircleFill />
+                    </Link>
+                }
+            </div>
+        </nav>
+
+    )
+}
